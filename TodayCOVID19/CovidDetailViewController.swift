c2e3f009d6c00d3e7624b06ce51f3db72e5fd496
choni <49021999+ryr0121@users.noreply.github.com>
2022-11-29 17:20:29 +0900

    Daily COVID19 Info APP
    당일 코로나19 관련 정보 제공 앱

--- TodayCOVID19/CovidDetailViewController.swift
+++ TodayCOVID19/CovidDetailViewController.swift
@@ -18,8 +18,22 @@ class CovidDetailViewController: UITableViewController {
     @IBOutlet weak var overseaInflowCell: UITableViewCell!
     @IBOutlet weak var regionalOutBreakCell: UITableViewCell!
     
+    var covidOverview: CovidOverview?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureView()
+    }
+    
+    func configureView() {
+        guard let covidOverview = self.covidOverview else { return }
+        self.title = covidOverview.countryName
+        self.newCaseCell.detailTextLabel?.text = "\(covidOverview.newCase)명"
+        self.todayCaseCell.detailTextLabel?.text = "\(covidOverview.totalCase)명"
+        self.recoveredCell.detailTextLabel?.text = "\(covidOverview.recovered)명"
+        self.deathCell.detailTextLabel?.text = "\(covidOverview.death)명"
+        self.percentageCell.detailTextLabel?.text = "\(covidOverview.percentage)%"
+        self.overseaInflowCell.detailTextLabel?.text = "\(covidOverview.newFcase)명"
+        self.regionalOutBreakCell.detailTextLabel?.text = "\(covidOverview.newCcase)명"
     }
 }
