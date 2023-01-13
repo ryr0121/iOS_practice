5908b1f371d01694c5b1a786f0bda6073514c7f2
chowon <ryr0121@gmail.com>
2023-01-13 16:07:26 +0900

    실시간 공지사항 앱

--- Notice/Notice/NoticeViewController.swift
+++ Notice/Notice/NoticeViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class NoticeViewController: UIViewController {
 
-    var noticeContents: (title: String, datail: String, date: String)?
-    
+    var noticeContents: (title: String, detail: String, date: String)?
+
     @IBOutlet weak var noticeView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var detailLabel: UILabel!
@@ -28,7 +28,7 @@ class NoticeViewController: UIViewController {
         
         guard let noticeContents = noticeContents else { return }
         self.titleLabel.text = noticeContents.title
-        self.detailLabel.text = noticeContents.datail
+        self.detailLabel.text = noticeContents.detail
         self.dateLabel.text = noticeContents.date
 
     }
