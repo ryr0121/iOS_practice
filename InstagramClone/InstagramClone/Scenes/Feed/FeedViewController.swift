88f2d555f7fc58847e5d375bbda54e63e51b9e30
chowon <ryr0121@gmail.com>
2023-01-26 10:17:48 +0900

    인스타그램 게시물 공유 화면 목업 및 기능 초안 구현

--- InstagramClone/InstagramClone/Scenes/Feed/FeedViewController.swift
+++ InstagramClone/InstagramClone/Scenes/Feed/FeedViewController.swift
@@ -20,6 +20,15 @@ class FeedViewController: UIViewController {
         return tableView
     }()
     
+    private lazy var imagePickerController: UIImagePickerController = {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+
+        return imagePickerController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -43,6 +52,27 @@ extension FeedViewController: UITableViewDataSource {
     }
 }
 
+extension FeedViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        var selectImage: UIImage?
+
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            selectImage = editedImage
+        } else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            selectImage = originalImage
+        }
+
+        picker.dismiss(animated: true) { [weak self] in
+//            let uploadViewController = UploadViewController()
+            let uploadViewController = UploadViewController(uploadImage: selectImage ?? UIImage())
+            let navigationController = UINavigationController(rootViewController: uploadViewController)
+            navigationController.modalPresentationStyle = .fullScreen
+
+            self?.present(navigationController, animated: true)
+        }
+    }
+}
+
 private extension FeedViewController {
     func setupNavigationBar() {
         navigationItem.title = "Outstagram"
@@ -51,11 +81,14 @@ private extension FeedViewController {
             image: UIImage(systemName: "plus.app"),
             style: .plain,
             target: self,
-            action: nil
+            action: #selector(didTapUploadButton)
         )
         navigationItem.rightBarButtonItem = uploadButton
     }
-
+    
+    @objc func didTapUploadButton() {
+        present(imagePickerController, animated: true)
+    }
 
     func setupTableView() {
         view.addSubview(tableView)
