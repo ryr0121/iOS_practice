eececb34d7d4865cf81a4874808a8a463dcba7ee
chowon <ryr0121@gmail.com>
2023-01-26 16:21:06 +0900

    GitHub 특정 Orgaization의 Repository 불러오는 앱

---
+++ GithubRepoClone/GithubRepoClone/Repository.swift
@@ -0,0 +1,21 @@
+//
+//  Repository.swift
+//  GithubRepoClone
+//
+//  Created by 김초원 on 2023/01/26.
+//
+
+import Foundation
+
+struct Repository: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let stargazersCount: Int
+    let language: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description, language
+        case stargazersCount = "stargazers_count"
+    }
+}
--- TableViewTest/TableViewTest/ViewController.swift
+++ TableViewTest/TableViewTest/ViewController.swift
@@ -17,3 +17,22 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 20.0
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
